508a53d8698f6b142a8baea540da978e82e4e5cc
RyTaus <rtaus1@lion.lmu.edu>
2018-02-15 16:40:42 -0800

    Set up package and made graphik.js export the project files

--- src/graphik.js
+++ src/graphik.js
@@ -6,19 +6,11 @@
 const Component = require('./component.js');
 const Canvas = require('./canvas.js');
 const Interaction = require('./interaction.js');
+const Template = require('./template.js');
 
-class Graphik {
-  constructor() {
-    this.name = 'hi';
-  }
-
-  addComponent(component) {
-    this.components.push(component);
-  }
-}
-
-Graphik.Component = Component;
-Graphik.Canvas = Canvas;
-Graphik.Interaction = Interaction;
-
-module.exports = Graphik;
+module.exports = {
+  Component,
+  Canvas,
+  Interaction,
+  Template
+};
